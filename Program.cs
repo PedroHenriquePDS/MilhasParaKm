b388ec9b7d384fbe4474ae0446417c2f7c30a3b2
Pedro Henrique <PedroHenrique053010@gmail.com>
2022-09-29 22:27:25 -0300

    Receba uma medida em milhas e exiba seu equivalente em quilômetros.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+double milhas, kms;
+
+Console.Write("Entre com a medida (em milhas): ");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+kms = milhas * 1.609;
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine($"{kms:N3} Km");
+Console.ResetColor();
+
+
